c3eab058b23725442d8c5c1c20b1b9cf07d3d83b
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-12-07 19:36:48 +1100

    Print out each line in proper format

--- Main.java
+++ Main.java
@@ -1,19 +1,15 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-	
-	
-	
+			
 	public static void main(String[] args) {
 	
 		//Filepath
 		String filePath = "records.csv";
 		
-			
 		try {
 			//Reader.
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
@@ -21,11 +17,30 @@ public class Main {
 			//Variable for temporarily storing each line.
 			String line = "";
 			
+			//Keep track of which line is being read.
+			int lineCounter = 0;
+			
 			//Loop over each line if the line is not null.
 			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-			}
-			
+				
+				if (lineCounter == 0) {
+					//If it is the first line then 
+					//just print "records:"
+					System.out.println("records:");
+				}
+				else {				
+					//Split each line with , into an array.
+					String[] columns = line.split(",");
+					
+					//Print out line details and use array index to return
+					//specific values.
+					System.out.println("- name: " + columns[0] + " " + columns[1]);
+					System.out.println("  details: In division " + columns[3] + 
+									   "  from " + columns[2] + 
+									   "  performing " + columns[5]);
+				}
+				lineCounter++;												
+			}			
 		} 
 		
 		//Handle Exceptions.
@@ -39,6 +54,5 @@ public class Main {
 			System.out.println("Unknown error");
 			System.out.println("Error code: " + e);
 		}
-
 	}
 }
