3f143b563af151dd210f5bbcca8eee2a92731ff1
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2021-12-07 19:24:29 +1100

    Handling all exceptions

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 	
@@ -13,9 +14,22 @@ public class Main {
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			String line = "";
 			
-		} catch (FileNotFoundException e) {
+			while ((line = reader.readLine()) != null) {
+				
+			}
+			
+		} 
+		catch (FileNotFoundException e) {
 			System.out.println("Invalid file path: " + filePath);
+		}
+		catch (IOException e) {
+			System.out.println("IOException error");
+		}
+		catch (Exception e) {
+			System.out.println("Unknown error");
+			System.out.println("Error code: " + e);
 		}
 
 	}
